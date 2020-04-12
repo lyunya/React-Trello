124ad6c20aba2e28c3f5803e905fb645c051f787
Leon Marbukh <leonmarbukh@gmail.com>
2020-04-11 17:00:06 -0700

    brought in state

--- src/App.js
+++ src/App.js
@@ -1,33 +1,40 @@
 import React from 'react';
 import './App.css';
 import List from './components/List';
+import STORE from './store';
 
-function App(props) {
-  const lists = props.store.lists.map((item, index) =>{
-  return <List key = {index} header = {item.header} cards = {getCards(item.cardIds)}/>
-  })
-  function getCards(cardNums){
+class App extends React.Component {
+  state = {
+    store: STORE,
+  };
+  
+  getCards = (cardNums) => {
     let result = [];
-      for(let i= 0; i < cardNums.length; i++){
-         const cardId = cardNums[i];
-         const card = props.store.allCards[cardId];
-         result.push(card);
-         console.log(result, 'leon');
-      }
-      
-    return result;
-  }
+        for (let i = 0; i < cardNums.length; i++) {
+          const cardId = cardNums[i];
+          const card = this.state.store.allCards[cardId];
+          result.push(card);
+        }
 
-  return (
-    <main className="App">
-    <header className="App-header">
-      <h1>Trelloyes!</h1>
-    </header>
-    <div className="App-list">
-      {lists}
-    </div>
-    </main>
-  );
+        return result;
+  };;
+
+  render() {
+    const lists = this.state.store.lists.map((item, index) => {
+      return (
+        <List key={index} header={item.header} cards={this.getCards(item.cardIds)} />
+      );
+    });
+
+    return (
+      <main className="App">
+        <header className="App-header">
+          <h1>Trelloyes!</h1>
+        </header>
+        <div className="App-list">{lists}</div>
+      </main>
+    );
+  }
 }
 
 export default App;
